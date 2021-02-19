196e464a40556a7a270811392c61aa6bcbcb07ca
varunchillara <varunkamesh22@gmail.com>
2021-02-18 19:06:26 -0500

    initial commit of project

---
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.udacity.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceApplication.class, args);
+    }
+}
